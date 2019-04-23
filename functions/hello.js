e690effa22dc0a73d7d2655bf05daea40973bb01
KunuTOK <kunutok@mail.ru>
2019-04-23 23:27:49 +0300

    Url

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context, callback) {
   const body =  JSON.parse(event.body)
 console.log(body.message.text)
+UrlFetchApp.fetch('https://api.telegram.org/bot' '742875963:AAEtJnKfKL8CjS9WRHJZnZHNo3Rhk2_Bo_8' '/', data);
   callback(null, {
     statusCode: 200,
     body: "today " + new Date()
