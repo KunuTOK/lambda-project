ef7a279f10e9a58e46f87b101837dcb7e0e9acff
JLarky <jlarky@gmail.com>
2019-04-23 13:23:51 -0700

    let's try inline reply to telegram hook

--- functions/hello.js
+++ functions/hello.js
@@ -1,8 +1,16 @@
 exports.handler = function(event, context, callback) {
-  const body =  JSON.parse(event.body)
-console.log(body.message.text)
+  const body = JSON.parse(event.body);
+  console.log(body.message.text);
+  var payload = {
+    method: "sendMessage",
+    chat_id: "@kunutok",
+    text: "testing reply " + body.message.text + " " + new Date(),
+    parse_mode: "HTML"
+  };
+
   callback(null, {
     statusCode: 200,
-    body: "today " + new Date()
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload)
   });
 };
