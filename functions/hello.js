b2f071a1d1ce52eaa9a03f2cc3ecc3f72c12cdbf
KunuTOK <kunutok@mail.ru>
2019-04-23 22:17:37 +0300

    consol.log

--- functions/hello.js
+++ functions/hello.js
@@ -1,4 +1,6 @@
 exports.handler = function(event, context, callback) {
+  const body =  JSON.parse(event.body)
+console.log(body.message.text)
   callback(null, {
     statusCode: 200,
     body: "today " + new Date()
