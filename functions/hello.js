0a7a908969e705add66143f05d645104602a985f
Dmitry <49252712+KunuTOK@users.noreply.github.com>
2019-05-16 01:05:56 +0300

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -2,10 +2,21 @@ exports.handler = function(event, context, callback) {
 
   const req = JSON.parse(event.body);
   console.log(req.message.text);
+
+var kst = new Date().toLocaleString("en-US", {timeZone: "Europe/Moscow"});
+      kst = new Date(kst);
+      console.log('Kostroma time: '+kst.toLocaleString())
+      document.write('Kostroma time: '+kst.toLocaleString())
+ document.write('<====>')
+ var y = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
+ y = new Date(y);
+ console.log('San Francisco time: '+y.toLocaleString())
+ document.write('San Francisco time: '+y.toLocaleString())
+
   var payload = {
     method: "sendMessage",
     chat_id: req.message.chat.id,
-    text: "testing reply " + req.message.chat.username + " " + new Date(),
+    text: 'Kostroma time: '+kst.toLocaleString() +'<====> '+'San Francisco time: '+y.toLocaleString()
     parse_mode: "HTML"
   };
 
