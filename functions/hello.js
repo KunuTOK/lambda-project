b13d4c5b69735047b24b9d4e712782a563dbf7b5
KunuTOK <kunutok@mail.ru>
2019-05-16 22:23:06 +0300

    time add

--- functions/hello.js
+++ functions/hello.js
@@ -2,13 +2,16 @@ exports.handler = function(event, context, callback) {
 
   const req = JSON.parse(event.body);
   console.log(req.message.text);
+  var kst = new Date().toLocaleString("en-US", {timeZone: "Europe/Moscow"});
+  kst = new Date(kst);
+  var y = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
+  y = new Date(y);
   var payload = {
     method: "sendMessage",
     chat_id: req.message.chat.id,
-    text: "testing reply " + req.message.chat.username + " " + new Date(),
+    text: req.message.chat.username + " exact time:\n" + "Kostroma: " + kst.toLocaleString()+ "" +'\n' + "San Francisco: "+ y.toLocaleString(),
     parse_mode: "HTML"
   };
-
   callback(null, {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
