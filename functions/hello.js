cc3f5e0dbd6b385fa5667f62b6d09d09a0948de7
KunuTOK <kunutok@mail.ru>
2019-05-23 00:09:07 +0300

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,11 +1,13 @@
 exports.handler = function(event, context, callback) {
 
   const req = JSON.parse(event.body);
- //console.log(req.message.text);
+
+console.log(req.message.text);
+
   var kst = new Date().toLocaleString("en-US", {timeZone: "Europe/Moscow"});
   kst = new Date(kst);
   var sf = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
-  y = new Date(sf);
+  sf = new Date(sf);
 
   var kostroma = {
     method: "sendMessage",
@@ -13,18 +15,27 @@ exports.handler = function(event, context, callback) {
     text: " Exact time:\n" + "Kostroma: " + kst.toLocaleString(),
     parse_mode: "HTML"
   };
+  var sanfran = {
+    method: "sendMessage",
+    chat_id: req.message.chat.id,
+    text: " Exact time:\n" + "San Francisco: " + sf.toLocaleString(),
+   parse_mode: "HTML"
+ };
 
-//  var payload = {
-//    method: "sendMessage",
-//    chat_id: req.message.chat.id,
-//    text: " Exact time:\n" + "San Francisco: " + sf.toLocaleString(),
-//    parse_mode: "HTML"
-//  };
-
-
+if (text === "/kostroma") {
   callback(null,  {
+ statusCode: 200,
+ headers: { "Content-Type": "application/json" },
+ body: JSON.stringify(sanfran)
+ });
+}
+
+if (req.message.text === "/sanfran") {
+
+} else if (req.message.text === '/kostroma') {
+   callback(null,  {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(kostroma)
-  });
+    });
 };
