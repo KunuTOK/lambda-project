21a5c2b93b785d2025c720e686b6f2ac953e5520
KunuTOK <kunutok@mail.ru>
2019-05-23 23:06:59 +0300

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,58 +1,28 @@
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
 
-  const req = JSON.parse(event.body);
+    const req = JSON.parse(event.body);
 
-console.log(req.message.text);
+    console.log(req.message.text);
 
-  var kst = new Date().toLocaleString("en-US", {timeZone: "Europe/Moscow"});
-  kst = new Date(kst);
-  var sf = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
-  sf = new Date(sf);
+    var kst = new Date().toLocaleString("en-US", { timeZone: "Europe/Moscow" });
+    kst = new Date(kst);
+    var sf = new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" });
+    sf = new Date(sf);
 
-  var payload = {
-     method: "sendMessage",
-     chat_id: req.message.chat.id,
-     text: " Exact time:\n" + "San Francisco: " + sf.toLocaleString(),
-     parse_mode: "HTML"
-   };
+    var payload = {
+        method: "sendMessage",
+        chat_id: req.message.chat.id,
+        text: " Exact time:\n" + "San Francisco: " + sf.toLocaleString(),
+        parse_mode: "HTML"
+    };
 
-   if (req.message.text === "/Kostroma") {
-     payload = kostroma
-   }
+    if (req.message.text === /\/Kostroma/) {
+        payload.text = " Exact time:\n" + "Kostroma: " + kst.toLocaleString();
+    }
 
-   callback(null,  {
-     statusCode: 200,
-     headers: { "Content-Type": "application/json" },
-     body: JSON.stringify(payload)
-   });
-
-//  var kostroma = {
-//    method: "sendMessage",
-//    chat_id: req.message.chat.id,
-//    text: " Exact time:\n" + "Kostroma: " + kst.toLocaleString(),
-//    parse_mode: "HTML"
-//  };
-//  var sanfran = {
-//    method: "sendMessage",
-//    chat_id: req.message.chat.id,
-//    text: " Exact time:\n" + "San Francisco: " + sf.toLocaleString(),
-  // parse_mode: "HTML"
-// };
-
-//if (text === "/kostroma") {
-//  callback(null,  {
- //statusCode: 200,
- //headers: { "Content-Type": "application/json" },
-// body: JSON.stringify(sanfran)
- //});
-//}
-
-//if (req.message.text === "/sanfran") {
-
-//} else if (req.message.text === '/kostroma') {
-//   callback(null,  {
-//    statusCode: 200,
-//    headers: { "Content-Type": "application/json" },
-//    body: JSON.stringify(kostroma)
-//    });
-//};
+    callback(null, {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload)
+    });
+}
